0254dc1c5854f6fc5c09110503664e6997b380c8
sagostinelli <batonenbois@gmail.com>
2020-03-29 19:41:52 -0400

    latest

--- hooks/build.sh
+++ hooks/build.sh
@@ -4,7 +4,7 @@ NODE_VERSION=$(echo $DOCKER_TAG | cut -d "-" -f2)
 
 if [ $DOCKER_TAG == "latest" ]
 then
-  docker build . --build-arg NODE_VERSION=latest -t ${IMAGE_NAME}
+  docker build . --build-arg NODE_VERSION=${DOCKER_TAG} -t ${IMAGE_NAME}
 else
   docker build . --build-arg NODE_VERSION=${NODE_VERSION} -t ${IMAGE_NAME}
 fi
